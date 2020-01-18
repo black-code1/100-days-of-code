e0a6a093dc3dc07ad22bce518ea5dcab60570bfe
black-code1 <legrandmahamat1@gmail.com>
2020-01-18 19:40:44 +0100

    Progressing with React

--- R1.js
+++ R1.js
@@ -625,4 +625,127 @@ class RenderInput extends React.Component {
   }
 };
 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+console.log('hello');
+    // change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: { this.state.activeUsers }</h1>
+      </div>
+    );
+  }
+};
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // change code below this line
+  componentDidMount() {
+document.addEventListener('keydown',this.handleKeyPress);
+  }
+  componentWillUnmount() {
+document.removeEventListener('keydown',this.handleKeyPress)
+  }
+  // change code above this line
+  handleEnter() {
+    this.setState({
+      message: this.state.message + 'You pressed the enter key! '
+    });
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+     // change code below this line
+    if (nextProps.value % 2 === 0 ) {
+    return true;
+    }
+     // change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>
+  }
+};
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState({
+      value: this.state.value + 1
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value}/>
+      </div>
+    );
+  }
+};
+
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color:"red", fontSize:16}}>Big Red</div>
+    );
+  }
+};
   */
--- r2-log.md
+++ r2-log.md
@@ -8,62 +8,66 @@ The log of my #100DaysOfCode challenge. Started on [January 01, Wednesday, 2020]
 
 Started Front End Libraries Certificate - Introduction to the Bootstrap Challenges to Add Font Awesome Icons to all of our ButtonsPassed
 
-### R1D2
+### R2D2
 
 Front End Libraries Certificate - Responsively Style Radio Buttons to Use Comments to Clarify Code
 
-### R1D3
+### R2D3
 
 Front End Libraries Certificate - Learn How Script Tags and Document Ready Work to Remove an Element Using jQuery
 
-### R1D4
+### R2D4
 
 Front End Libraries Certificate - Ends jQuery challenge && Start Sass
 
-### R1D5
+### R2D5
 
 Front End Libraries Certificate - Ends Sass challenge
 
-### R1D6
+### R2D6
 
 Front End Libraries Certificate - Start React
 
-### R1D7
+### R2D7
 
 Front End Libraries Certificate - Continue with React
 
-### R1D8
+### R2D8
 
 Front End Libraries Certificate - Continue with React
 
-### R1D9
+### R2D9
 
 Front End Libraries Certificate - Continue with React
 
-### R1D10
+### R2D10
 
 Front End Libraries Certificate - Continue with React
 
-### R1D11
+### R2D11
 
 Front End Libraries Certificate - Continue with React
 
-### R1D12
+### R2D12
 
 Front End Libraries Certificate - Continue with React
 
-### R1D13
+### R2D13
 
 Completing JS challenges because of the recent freecodecamp update which undo some my completed challenges
 
-### R1D14
+### R2D14
 
 Found the solution to my react code not working, it was a browser issue
 
-### R1D15
+### R2D15
 
 Front End Libraries Certificate - Continue with React.
 
-### R1D16
+### R2D16
+
+Front End Libraries Certificate - Progressing with React.
+
+### R2D17
 
 Front End Libraries Certificate - Progressing with React.
